7c86299e5b0b9634b18242c7f4dfda01f938e19a
Santoshpro <104438729+Santoshpro@users.noreply.github.com>
2022-05-27 15:07:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function validate()
 {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    if(username=="abc" && password=="123")
+    if(username=="user1" && password=="123456789")
    {
        alert("login sucessfully");
    }
